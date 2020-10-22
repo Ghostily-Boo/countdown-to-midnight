23cf43f06addd18cb267442b4078591f285080ca
Ghostily-Boo <grace.rl.qin@gmail.com>
2020-10-22 11:22:32 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,26 @@
-#write your code here
 
-def countdown
+
+def countdown(seconds)
+    loop do
+        puts "#{seconds} SECOND(S)!"
+        seconds -= 1
+        if seconds <= 0
+            break
+        end
+    end
+    return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(seconds)
+    loop do
+        puts "#{seconds} SECOND(S)!"
+        seconds -= 1
+        sleep(1)
+        if seconds <= 0
+            break
+        end
+    end
+    return "HAPPY NEW YEAR!"
+end
+
+
